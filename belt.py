3bd59a9ccb271f52b6763f134b8c7b8d11f02c67
Eli Ellis <me@eliellis.com>
2013-03-08 00:28:04 -0500

    initial

---
+++ belt.py
@@ -0,0 +1,23 @@
+class Toolbelt(object):
+
+    # def __init__(self):
+        # not much to initialize at this point
+
+    def reduce(self, fn):
+        def iterate(*args):
+            i = iter(args)
+            try:
+                start = next(i)
+            except Exception, e:
+                raise e
+            val = start
+            for item in i:
+                val = fn(val, item)
+            return val
+
+        return iterate
+
+    def map(self, fn):
+        def iterate(*args):
+            return [fn(i) for i in args]
+        return iterate
